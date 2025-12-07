6b51e74a3bb7be15985fb33ef3f9104850e3fbf5
abcd <abcd@gmail.com>
2025-12-07 16:31:41 +0500

    added ci/cd

---
+++ src/__tests__/AddTransaction.test.js
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { AddTransaction } from '../components/AddTransaction';
+import { GlobalProvider } from '../context/GlobalState';
+
+test('renders add transaction form', () => {
+  render(<GlobalProvider><AddTransaction /></GlobalProvider>);
+  
+  const textInput = screen.getByPlaceholderText(/Enter text/i);
+  const amountInput = screen.getByPlaceholderText(/Enter amount/i);
+  const button = screen.getByText(/Add transaction/i);
+
+  expect(textInput).toBeInTheDocument();
+  expect(amountInput).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
+
+  fireEvent.change(textInput, { target: { value: 'Pizza' } });
+  fireEvent.change(amountInput, { target: { value: '200' } });
+  fireEvent.click(button);
+});
